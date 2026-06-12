d6b5975243ffab5fe658e929cdaffcec5551beb9
concego <euconcego@gmail.com>
2026-06-12 15:13:48 -0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'codice-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './assets/css/style.css',
+    './assets/js/app.js',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
